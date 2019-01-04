6d6c63a59e53128b87672cfe285aa30357f95a0e
autocommit <autocommit>
2019-01-04 18:08:43 +0000

    auto 0 29 395

---
+++ generate_dest_directory.cmake
@@ -0,0 +1,9 @@
+function(generate_dest_directory varname file)
+	get_filename_component(dir "${dest}" DIRECTORY)
+	get_filename_component(dir "${dir}" ABSOLUTE
+		BASE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+	add_custom_command(
+		OUTPUT "${dir}"
+		COMMAND mkdir "${dir}")
+	set("${varname}" "${dir}" PARENT_SCOPE)
+endfunction(generate_dest_directory)
