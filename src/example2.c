e30baddb517227afc432aeeec464757123ce39e7
autocommit <autocommit>
2019-05-28 19:21:13 +0000

    auto 0 19 109

--- src/example2.c
+++ src/example2.c
@@ -1,9 +1,13 @@
-{(C void main(void) {
-			int foo = 42;
-			)}
-	{(c "%d", foo)};
-	{(c "%d", foo)};
-	{(c "%d", foo)};
-	{(C
-				}
-		)}
+{|C
+	void main(void) {
+	int foo = 42;
+	|}
+	{|c "%d", foo|};
+int a = 3;
+	{|c "%d", foo|};
+int b = 4;
+	{|c "%d", foo|};
+int c = 5;
+	{|C
+			}
+	|}
