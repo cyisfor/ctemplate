bf2d686c589e66ac814471e98c26b69566771fed
autocommit <autocommit>
2019-05-29 01:33:57 +0000

    auto 0 12 162

--- src/example2.c
+++ src/example2.c
@@ -1,11 +1,15 @@
 .|C
+#include <stdio.h> // 
+
 #include "output.h"
 int main(void) {
 	int foo = 5;
 	out = stdout;
-	|.
+	puts("This is compile time, making the runtime code.");
+	|.#include <stdio.h>
+			
 		int main(void) {
-			puts("runtime after template goes here.");
+			puts("runtime out of template goes here.");
 			int bar = .|c "%d", foo|.;
 			printf("in runtime, bar is %d\n", bar);
 			printf("in compile time, foo is .|c "%d", foo|.\n");
