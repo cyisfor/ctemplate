069fdc3605e89d6c7af8ecec8cdb7a3375e3856f
autocommit <autocommit>
2019-05-29 01:13:00 +0000

    auto 0 84 998

--- src/example2.c
+++ src/example2.c
@@ -1,13 +1,23 @@
 ;|C
-void main(void) {
+int main(void) {
 	int foo = 42;
 	|;
-	;|c "%d", foo|;;
-	int a = 3;
-	;|c "%d", foo|;;
-	int b = 4;
-	;|c "%d", foo|;
-	int c = 5;
-	;|C ;
+	int main(void) {
+		puts("runtime after template goes here.");
+		int bar = ;|c "%d", foo|;;
+		printf("in runtime, bar is %d\n", bar);
+		printf("in compile time, foo is ;|c "%d", foo|;\n");
+		int i;
+		for(i=0;i<bar;++i) {
+			puts("foo");
+		}
+		;|C
+				int i;
+				for(i=0;i<foo;++i) {
+					|;puts("bar");;|;
+				}
+				|;
 	}
+	;|
+		}
 |;
