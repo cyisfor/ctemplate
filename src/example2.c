c450c9f5b3e8e325667d60bddb6560374f8e0a69
user <email>
2019-05-29 00:46:01 +0000

    derp

--- src/example2.c
+++ src/example2.c
@@ -1,13 +1,13 @@
-{|C
-	void main(void) {
+;|C
+void main(void) {
 	int foo = 42;
-	|}
-	{|c "%d", foo|};
-int a = 3;
-	{|c "%d", foo|};
-int b = 4;
-	{|c "%d", foo|};
-int c = 5;
-	{|C
-			}
-	|}
+	|;
+	;|c "%d", foo|;;
+	int a = 3;
+	;|c "%d", foo|;;
+	int b = 4;
+	;|c "%d", foo|;
+	int c = 5;
+	;|C ;
+	}
+|;
